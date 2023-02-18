199ed2e1c31059ff62eed3eb2378ffaa0de4555c
Emerson Pessoa <60630092+emersonpessoa01@users.noreply.github.com>
2023-02-18 12:25:21 -0300

    upload init :sparkles:

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PaginaNaoEncontradaComponent } from "./pagina-nao-encontrada/pagina-nao-encontrada.component";
+import { NgModule, Component } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { SegundaPaginaComponent } from "./segunda-pagina/segunda-pagina.component";
+import { PrimeiraPaginaComponent } from "./primeira-pagina/primeira-pagina.component";
+import { PaginaComParametrosComponent } from "./pagina-com-parametros/pagina-com-parametros.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: "primeira-pagina",
+        component: PrimeiraPaginaComponent,
+    },
+    {
+        path: "segunda-pagina",
+        component: SegundaPaginaComponent,
+    },
+
+    {
+        path: "pagina-com-parametros/:id",
+        component: PaginaComParametrosComponent,
+    },
+    {
+        path: "**",
+        component: PaginaNaoEncontradaComponent,
+    },
+    {
+        path: "",
+        redirectTo: "primeira-pagina",
+        pathMatch: "full",
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { PrimeiraPaginaComponent } from "./primeira-pagina/primeira-pagina.component";
+import { SegundaPaginaComponent } from "./segunda-pagina/segunda-pagina.component";
+import { PaginaNaoEncontradaComponent } from "./pagina-nao-encontrada/pagina-nao-encontrada.component";
+import { PaginaComParametrosComponent } from "./pagina-com-parametros/pagina-com-parametros.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        PrimeiraPaginaComponent,
+        SegundaPaginaComponent,
+        PaginaNaoEncontradaComponent,
+        PaginaComParametrosComponent,
+    ],
+    imports: [BrowserModule, AppRoutingModule],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
